01a4df027b27529c244a55ad6bd7d51422882e6b
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-09-23 00:16:11 +0300

    Initial Commit

---
+++ BetterNavigation/BetterNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterNavigationApp.swift
+//  BetterNavigation
+//
+//  Created by Abdelrahman Mohamed on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
